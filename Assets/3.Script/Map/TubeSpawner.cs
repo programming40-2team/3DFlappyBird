e8f2d1739e490c3641fa85e194d64c0d158ae5da
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-05-03 20:17:14 +0900

    맵

---
+++ Assets/3.Script/Map/TubeSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TubeSpawner : MonoBehaviour
+{
+    private ObjectPool objectPool;
+    private float randomNum;
+    [SerializeField] private float spawnTime;
+
+    private void Start()
+    {
+        GameObject.FindGameObjectWithTag("TubePool").TryGetComponent(out objectPool);
+        StartCoroutine(SpawnTube_co());
+    }
+
+    private void SetRandomPosition()
+    {
+        randomNum = Random.Range(9f, 23f);
+    }
+
+    private IEnumerator SpawnTube_co()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(spawnTime);
+        while(true)
+        {
+            SetRandomPosition();
+            GameObject obj = objectPool.GetObject();
+            obj.transform.position = new Vector3(transform.position.x, randomNum, transform.position.z);
+            yield return wfs;
+        }
+
+    }
+}
