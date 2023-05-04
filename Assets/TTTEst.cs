a1bf478e4c6e5907270d86feeee844872c026b13
00ill <cyi1769@naver.com>
2023-05-04 13:52:17 +0900

    no message

--- Assets/TTTEst.cs
+++ Assets/TTTEst.cs
@@ -18,6 +18,8 @@ public class TTTEst : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("ÀÌ·¸¤¡¤Ä");
+        Debug.Log("good");
     }
+
 }
