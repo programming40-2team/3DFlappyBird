e39c82f2c75c96757e9278aabd4cb80f466d175d
00ill <cyi1769@naver.com>
2023-05-04 17:02:11 +0900

    오류 수정

--- Assets/3.Script/Map/Movement3D.cs
+++ Assets/3.Script/Map/Movement3D.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Movement3D : MonoBehaviour
 {
     [SerializeField] private Vector3 moveDirection = Vector3.zero;
-    [SerializeField] private float moveSpeed;
+    private float moveSpeed;
+    private void Awake()
+    {
+        moveSpeed = 8;
+    }
 
     private void Update()
     {
+
         gameObject.transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 }
